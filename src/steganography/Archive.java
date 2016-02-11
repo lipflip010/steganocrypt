5c503dc46902dd79fd15f021b725cd01fc8df0b8
1ipf1ip <lipflip.albers@gmail.com>
2016-02-11 19:47:17 +0100

    deleted old method version

--- src/steganography/Archive.java
+++ src/steganography/Archive.java
@@ -22,12 +22,13 @@ import java.util.zip.GZIPOutputStream;
 
 public class Archive {
 
-	private static final String SOURCE = "G:\\Dokumente\\Workspace\\stenography\\src\\ananas.ai";
-	private static final String MESSAGE = "G:\\Dokumente\\Workspace\\stenography\\src\\Secret_Message.txt";
-	private static final String GZIP_FILE = "G:\\Dokumente\\Workspace\\stenography\\src\\Secret_Message.gz";
-	private static final String MESSAGE_DECOMPRESSED = "G:\\Dokumente\\Workspace\\stenography\\src\\Secret_Message_decompressed.txt";
+	private static final String SOURCE = "G:\\Dokumente\\Workspace\\steganography\\src\\hans.png";
+	private static final String MESSAGE = "G:\\Dokumente\\Workspace\\steganography\\src\\Secret_Message.txt";
+	private static final String GZIP_FILE = "G:\\Dokumente\\Workspace\\steganography\\src\\Secret_Message.gz";
+	private static final String MESSAGE_DECOMPRESSED = "G:\\Dokumente\\Workspace\\steganography\\src\\Secret_Message_decompressed.txt";
 	final protected static char[] hexArray = "0123456789abcdef".toCharArray();
 
+	
 	public void compress() {
 
 		byte[] buffer = new byte[1024];
@@ -123,57 +124,7 @@ public class Archive {
 
 	}
 
-	public void extractGZIP_old() {
-		byte[] buffer = new byte[1024];
-		boolean gzip_magic = false;
-		String byte_old = "";
-		String byte_current;
-		
-		try {
 
-			FileInputStream in = new FileInputStream(SOURCE);
-			FileOutputStream out = new FileOutputStream(GZIP_FILE);
-			// GZIPInputStream gzis = new GZIPInputStream(new
-			// FileInputStream(GZIP_FILE));
-			int len;
-		
-
-			while ((len = in.read(buffer)) > 0) {
-
-				for (int i = 0; i < len; i++) {
-					byte_current = String.format("%02x", buffer[i]);
-					if (byte_old.equals("1f") && byte_current.equals("8b")) {
-						out.write(buffer[i - 1]);
-						gzip_magic = true;
-					}
-					if (gzip_magic) {
-						out.write(buffer[i]);
-					}
-					// if (i%2 == 0) {
-					// System.out.print(byte_current);
-					// }
-					// else
-					// {
-					// System.out.print(byte_current+" ");
-					// }
-					byte_old = byte_current;
-				}
-			}
-
-			String log = (gzip_magic) ? "GZIP found and extracted to: " + GZIP_FILE : "No GZIP found";
-			System.out.println(log);
-
-			in.close();
-			out.close();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-	}
 
 	public void decompress() {
 		byte[] buffer = new byte[1024];
